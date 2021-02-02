7dd3c60552e199aabd5939ead0cc1e44f2ccd5bf
hublad <hublad@wp.pl>
2021-02-02 12:07:30 +0100

    Basic styling, routing

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-import { route } from './router';
+import { route } from "./router";
 
-route('/', 'home', function() {
-  this.where = 'here';
+route("/", "home", function () {
+  this.where = "here";
+  this.$on(".form__submit", "click", (e) => {
+    e.preventDefault();
+    console.log(e.view.location.origin);
+    window.location = `${e.view.location.origin}/#/ex2`;
+  });
 });
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
+route("/ex1", "example1", function () {
+  this.title = "Example 1";
 });
 
-route('/ex2', 'example2', function() {
-  this.title = 'Example 2';
+route("/ex2", "example2", function () {
+  this.title = "Example 2";
   this.counter = 0;
-  this.$on('.my-button', 'click', () => {
-    this.counter += 1;
+  this.$on(".my-button", "click", () => {
+    this.counter += 2;
     this.$refresh();
   });
 });
 
-route('*', '404', function () {});
+route("*", "404", function () {});
